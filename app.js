cf35a4214ac004769b671284db7dfd2092eed785
Emithen <86219540+Emithen@users.noreply.github.com>
2025-02-24 02:54:55 +0900

    Init

---
+++ app.js
@@ -0,0 +1,74 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+
+// prettier-ignore
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '','','','','','','','',
+    '','','','','','','','',
+    '','','','','','','','',
+    '','','','','','','','',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+];
+
+function createBoard() {
+  console.log("보드 초기화");
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerHTML = startPiece;
+    square.firstChild && square.firstChild.setAttribute("draggable", true);
+    square.setAttribute("square-id", i);
+
+    // 색칠
+    const row = Math.floor((63 - i) / 8) + 1;
+    if (row % 2 === 0) {
+      square.classList.add(i % 2 === 0 ? "beige" : "brown");
+    } else {
+      square.classList.add(i % 2 === 0 ? "brown" : "beige");
+    }
+
+    // 진영 추가
+    if (i <= 15) {
+      square.firstChild.firstChild.classList.add("black");
+    }
+
+    if (i >= 48) {
+      square.firstChild.firstChild.classList.add("white");
+    }
+
+    gameBoard.append(square);
+  });
+}
+
+createBoard();
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+allSquares.forEach((square) => {
+  square.addEventListener("dragstart", dragStart);
+  square.addEventListener("dragover", dragOver);
+  square.addEventListener("drop", dragDrop);
+});
+
+let startPositionId;
+let draggedElement;
+
+function dragStart(e) {
+  startPositionId = e.target.parentNode.getAttribute("square-id");
+  draggedElement = e.target;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop(e) {
+  e.stopPropagation();
+
+  //e.target.parenNode.append(draggedElement);
+  e.target.append(draggedElement);
+}
